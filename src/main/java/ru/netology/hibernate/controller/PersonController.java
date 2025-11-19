d0f4ca13cb32a05a4231cb1f1cdd9b7d7a9aa03c
Keni <petryakov8@gmail.com>
2025-11-19 14:20:34 +0400

    implement JPA repository with query methods

--- src/main/java/ru/netology/hibernate/controller/PersonController.java
+++ src/main/java/ru/netology/hibernate/controller/PersonController.java
@@ -7,6 +7,7 @@ import ru.netology.hibernate.entity.Person;
 import ru.netology.hibernate.repository.PersonRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -16,8 +17,23 @@ public class PersonController {
         this.repository = repository;
     }
 
+    // GET /persons/by-city?city=Moscow
     @GetMapping("/persons/by-city")
     public List<Person> getPersonsByCity(@RequestParam String city) {
-        return repository.getPersonsByCity(city);
+        return repository.findByCityOfLiving(city);
+    }
+
+    // GET /persons/by-age?age=30
+    @GetMapping("/persons/by-age")
+    public List<Person> getPersonsByAgeLessThan(@RequestParam int age) {
+        return repository.findPersonsByAgeLessThanOrderByAge(age);
+    }
+
+    // GET /persons/by-name-surname?name=Ivan&surname=Ivanov
+    @GetMapping("/persons/by-name-surname")
+    public Optional<Person> getPersonByNameAndSurname(
+            @RequestParam String name,
+            @RequestParam String surname) {
+        return repository.findByIdNameAndIdSurname(name, surname);
     }
 }
--- src/main/java/ru/netology/hibernate/repository/PersonRepository.java
+++ src/main/java/ru/netology/hibernate/repository/PersonRepository.java
@@ -1,23 +1,25 @@
 package ru.netology.hibernate.repository;
 
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import ru.netology.hibernate.entity.Person;
+import ru.netology.hibernate.entity.PersonId;
 
 import java.util.List;
 
 @Repository
-public class PersonRepository {
+public interface PersonRepository extends JpaRepository<Person, PersonId>{
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    List<Person> findByCityOfLiving(String city);
 
-    public List<Person> getPersonsByCity(String city) {
-        return entityManager.createQuery(
-                        "SELECT p FROM Person p WHERE p.cityOfLiving = :city", Person.class)
-                .setParameter("city", city)
-                .getResultList();
-    }
+    List<Person> findByAgeLessThanOrderByAge(String name, String surname, int age);
+
+    @Query("SELECT p FROM Person p WHERE p.id.age < :age ORDER BY p.id.age ASC")
+    List<Person> findPersonsByAgeLessThanOrderByAge(@Param("age") int age);
+
+    Optional<Person> findByIdNameAndIdSurname(String name, String surname);
 }
